6d18af24939ce472e45e6023f22f3c470b7e0a54
Kirat Bains <kiratbains09@gmail.com>
2023-07-22 22:30:58 -0700

    feat:main file update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
 def play():
-    user = input("'r' for rock, 'p' for paper, 's for scissors'" )
+    user = input("What is your choice? 'r' for rock, 'p' for paper, 's for scissors'" )
     computer = random.choice(['r', 'p', 's'])
     
     if user == computer:
